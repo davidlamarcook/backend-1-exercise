12c567c37790515a0c4d2812744aadb03426723c
davidlamarcook <dcook1025@gmail.com>
2022-05-19 18:24:40 -0600

    fixed backend-1

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ app.use(cors());
 
 app.get("/api/users", (req, res) => {
     let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
-    res.status(200).send(phrase)
+    res.status(200).send(friends)
 });
 
-app.get("/api/users", (req, res) => {
+app.get("/weather/:temperature", (req, res) => {
     const { temperature } = req.params
     const phrase = `<h3>It was ${temperature} today</h3>`
     res.status(200).send(phrase);
-})
+});
 
 app.listen(4000, () => console.log('Server running on 4000'))
